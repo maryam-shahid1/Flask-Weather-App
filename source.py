69ba8a34ca6d1ccf3465a1a22a5e52b8c158156b
Maryam Shahid <maryam.shahid@arbisoft.com>
2023-07-11 12:33:57 +0500

    code refactoring

--- source.py
+++ source.py
@@ -2,8 +2,9 @@ from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
-  
-@app.route('/', methods= ["GET", "POST"])
+
+
+@app.route('/', methods=["GET", "POST"])
 def index(): 
     weatherData = ''
     cityName = ''
@@ -11,11 +12,13 @@ def index():
         cityName = request.form.get("cityName")
         print(cityName)
         if cityName:
-            weatherApiKey = 'bdb7a8298ce91213e409098d9afd03b0'
-            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + weatherApiKey
+            weatherApiKey = 'api_key'
+            url = "https://api.openweathermap.org/data/2.5/weather?q="
+            + cityName + "&appid=" + weatherApiKey
             weatherData = requests.get(url).json()
             print(weatherData)
     return render_template('index.html', data=weatherData)
+
 
 if __name__ == "__main__":
     app.run()
